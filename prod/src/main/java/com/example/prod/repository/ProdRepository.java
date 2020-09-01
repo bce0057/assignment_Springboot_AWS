8e313f5b2875b985a52a79408581c18bb1e49eab
Giri <SaiVishnu.Giri@thomsonreuters.com>
2020-09-02 01:55:32 +0530

    removing files

--- prod/src/main/java/com/example/prod/repository/ProdRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.prod.repository;
-
-import com.example.prod.model.Prod;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProdRepository extends CrudRepository<Prod,String> {
-}
